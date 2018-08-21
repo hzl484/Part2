3f13463787992d0c87502cbe03f0598901cc3809
何宗林 <772545428@qq.com>
2018-08-21 17:22:14 +0800

    搭建框架更正：customer实体类加上用户名、密码和姓名

--- src/main/java/cn/gezhi/po/Customer.java
+++ src/main/java/cn/gezhi/po/Customer.java
@@ -3,6 +3,12 @@ package cn.gezhi.po;
 public class Customer {
     private Integer id;
 
+    private String username;
+
+    private String password;
+
+    private String name;
+
     private String number;
 
     private String idcardnum;
@@ -39,6 +45,30 @@ public class Customer {
 
     public void setId(Integer id) {
         this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username == null ? null : username.trim();
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password == null ? null : password.trim();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
     }
 
     public String getNumber() {
--- src/main/java/cn/gezhi/service/SalesmanService.java
+++ src/main/java/cn/gezhi/service/SalesmanService.java
@@ -1,4 +1,14 @@
 package cn.gezhi.service;
 
+import cn.gezhi.po.Customer;
+import cn.gezhi.po.Salesman;
+
+import java.util.List;
+
 public interface SalesmanService {
+    //业务员登录
+    Salesman salesManLogin(String username,String password);
+    //业务员界面显示客户列表
+    List<Customer> showCustomer();
+
 }
